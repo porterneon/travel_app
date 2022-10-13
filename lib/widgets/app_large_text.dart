c0c7e31d66349d3f5ff67a670e590efb34b8c9f7
Marcin Wielgus <porterneon@gmail.com>
2022-10-13 21:24:54 +0200

    initial commit with first page of slider

---
+++ lib/widgets/app_large_text.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppLargeText extends StatelessWidget {
+  double size;
+  final String text;
+  Color color;
+
+  AppLargeText({
+    super.key,
+    this.size = 30,
+    required this.text,
+    this.color = Colors.black,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: color,
+        fontSize: size,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
