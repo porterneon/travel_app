93cc08c987e750fe3629f7c1c947a006a49e9e7e
Marcin Wielgus <porterneon@gmail.com>
2022-12-10 22:57:23 +0100

    add detail page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:travel_app/pages/navpages/main_page.dart';
+import 'package:travel_app/pages/detail_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MainPage(),
+      home: const DetailPage(),
     );
   }
 }
